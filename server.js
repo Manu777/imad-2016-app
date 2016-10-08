60b9b83cf816214818669a4b5f22c2f50c1642ea
Manu777 <mehulj5775@gmail.com>
2016-10-08 22:08:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,6 +121,21 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var names = [];
+app.get('/submit-name/:name', function (req, res){
+   //get the name from the request object
+   var name = req.params.name;
+   var name; //T000
+   //now we need to concatenate to our overall list
+   names.push(name);
+   //JSON - Javascript Object Notation - a way of converting js objects into strings
+   res.send(JSON.stringify(names));
+   //how do we extract the name from the request
+   //for this, we will send the data as part of the url object
+   
+   //how are we gonna send the name, as name is in aaray and this is javacsript object
+   //that is how can we send a javascript object as an array
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
