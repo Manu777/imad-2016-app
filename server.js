59f0c5a12980988247f285579e8fe6698d9456bb
Manu777 <mehulj5775@gmail.com>
2016-10-08 11:59:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,10 +93,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter = 0;
+var counter = 0; //initially
 app.get('/counter', function (req,res){
     counter = counter + 1;
-    res.send(counter.toString());
+    res.send(counter.toString()); //only string can be sent
 });
 
 
