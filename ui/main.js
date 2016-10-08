38d3e94f7cf59ba1c69886b4b504c3c3beedc2cb
Manu777 <mehulj5775@gmail.com>
2016-10-08 21:23:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ button.onclick = function() {
     };
     
     //make request
-    request.open('GET', 'http://coco98.imad.hasura-app.io/counter',true);
+    request.open('GET', 'http://manu777.imad.hasura-app.io/counter',true);
     request.send(null);
     
    
