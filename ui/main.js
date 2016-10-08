e46d4b6396df6bc7c013754796ac5ec810d76558
Manu777 <mehulj5775@gmail.com>
2016-10-08 22:36:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,8 +31,7 @@ button.onclick = function() {
 
 //submit name
 
-var nameInput = document.getElementById('name');
-var name      = nameInput.value;
+
 var submit    = document.getElementById('submit-btn');
 submit.onclick = function() {
     //make a request to a server and send the name
@@ -63,6 +62,8 @@ submit.onclick = function() {
     };
     
     //make request
+    var nameInput = document.getElementById('name');
+var name      = nameInput.value;
     request.open('GET', 'http://manu777.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
     
